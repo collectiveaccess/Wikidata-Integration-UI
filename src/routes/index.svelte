b6e4e5fa36c257013f94e6ace541c0032d1f0550
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-14 11:28:11 -0700

    create standalone searchable graph network page; delete unused routes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,143 +1,156 @@
 <script>
   import AutoComplete from 'simple-svelte-autocomplete';
   import { onMount } from 'svelte';
-  import Claim from '$lib/components/claim.svelte';
-  import ItemBasicInfo from '$lib/components/item_basic_info.svelte';
-  import { searchKeyword, fetchWikidataItem } from '$lib/common/wiki_queries';
 
-  let testIds = {
-    Q5: 'human',
-    Q30: 'United States', // place
-    Q487604: 'Martha Graham', // dancing digital
-    Q16973731: 'Dianne McIntyre', // dancing digital
-    Q753828: 'Essex', // place
-    Q76: 'Barack Obama', // has lexeme
-    Q28425: 'Chiroptera', // has video, sound, images
-    Q111420520: 'Karl Hirsch'
-  };
-  let testId = null;
+  import {
+    searchKeyword,
+    allMenuOptions,
+    getNetworkGraphData,
+    formatNetworkGraphDataForVisJs
+  } from '$lib/common/wiki_queries';
+  import NetworkGraph from '$lib/components/network_graph.svelte';
+  import { martha } from '$lib/data/networks_raw.js';
 
-  let searchItem = '';
-  let currentItem = null;
-  let currentId = testId;
-  let currentLabel = testIds[testId];
-  let loading = false;
-  let languageCodesDisplay = [];
+  let networkData = {};
 
   // ====================
-  // display record
+  // select properties
   // ====================
+  let properties = [].concat(...Object.values(allMenuOptions)).map((o) => o['id']);
+  let iterations = 1;
+  let destroyGraph = false;
 
-  function setlanguageCodesDisplay(item) {
-    let languages = item['languages'];
-    let tmp = Object.keys(languages);
-    // ensure 'en' is first language shown
-    if (languages['en']) {
-      let en_idx = tmp.indexOf('en');
-      tmp = ['en', ...tmp.slice(0, en_idx), ...tmp.slice(en_idx + 1)];
-    }
+  async function submitQuery() {
+    if (!searchItem['id']) return;
+    destroyGraph = false;
 
-    languageCodesDisplay = tmp;
+    networkData = {};
+    networkData = await getNetworkGraphData([searchItem['id']], properties, iterations);
   }
 
-  function displayItem(item) {
-    setlanguageCodesDisplay(item);
-  }
-
-  function resetSearch() {
-    currentItem = null;
-    currentId = null;
-    currentLabel = null;
-    searchItem = '';
+  function resetQuery() {
+    searchItem = {};
+    networkData = {};
+    properties = [].concat(...Object.values(allMenuOptions)).map((o) => o['id']);
+    iterations = 1;
+    destroyGraph = true;
   }
 
   // ====================
   // autocomplete
   // ====================
+  let searchItem = {};
+  let itemId = null;
+  let itemLabel = null;
 
   async function loadOptions(keyword) {
+    if (keyword) {
+      itemId = null;
+      itemLabel = null;
+    }
+
     if (keyword.length > 1) {
-      let json = searchKeyword(keyword);
+      let json = await searchKeyword(keyword);
       return json;
     }
   }
 
   async function handleSelect(selectedOption) {
-    if (!selectedOption) return;
-    loading = true;
-    currentItem = null;
-    currentId = selectedOption['id'];
-    currentLabel = selectedOption['label'];
-    currentItem = await fetchWikidataItem(currentId);
-    displayItem(currentItem);
-    loading = false;
+    if (Object.keys(selectedOption).length == 0) return;
+
+    itemId = selectedOption['id'];
+    itemLabel = selectedOption['label'];
   }
+
+  // ====================
+  // misc
+  // ====================
+  function preloadRecord() {
+    itemId = 'Q487604';
+    itemLabel = 'Martha Graham';
+    searchItem = { id: itemId, label: itemLabel };
+    networkData = formatNetworkGraphDataForVisJs(martha);
+  }
+
 
   // ====================
   // life cycle
   // ====================
-
   onMount(async () => {
-    if (currentId) {
-      loading = true;
-      currentItem = await fetchWikidataItem(currentId);
-      displayItem(currentItem);
-      loading = false;
-    }
+    // preloadRecord();
   });
 </script>
 
-<h1 class="title is-1">Wikidata Demo</h1>
-
+<h1 class="title is-1">Search Wikidata.org</h1>
 <div class="columns">
-  <div class="column is-four-fifths">
-    <AutoComplete
-      searchFunction={loadOptions}
-      delay="200"
-      onChange={handleSelect}
-      labelFieldName="search_label"
-      placeholder="Search keyword"
-      hideArrow={true}
-      showClear={false}
-      localFiltering={false}
-      bind:selectedItem={searchItem}
-    />
+  <div class="column is-one-third explorer-menu">
+    <div class="field">
+      <label class="label" for="search">Search</label>
+      <AutoComplete
+        searchFunction={loadOptions}
+        delay="200"
+        onChange={handleSelect}
+        labelFieldName="search_label"
+        placeholder="Search keyword"
+        hideArrow={true}
+        showClear={false}
+        localFiltering={false}
+        bind:selectedItem={searchItem}
+      />
+    </div>
+
+    <div class="field">
+      {#each Object.entries(allMenuOptions) as [menuType, options]}
+        <div class="menu-type">{menuType}</div>
+        {#each options as option (option['id'])}
+          <label class="checkbox">
+            <input
+              type="checkbox"
+              name="property_type"
+              bind:group={properties}
+              value={option['id']}
+            />{option['label']}
+          </label><br />
+        {/each}
+      {/each}
+    </div>
+
+    <div class="field">
+      <label class="label" for="iterations"> Iterations</label>
+      <div class="control">
+        <input id="iterations" type="number" bind:value={iterations} name="iterations" min="1" />
+      </div>
+    </div>
+
+    <div class="field is-grouped">
+      <div class="control">
+        <button on:click={submitQuery} class="button is-link">Submit</button>
+      </div>
+      <div class="control">
+        <button on:click={resetQuery} class="button is-link is-light">Reset</button>
+      </div>
+    </div>
   </div>
-  <div class="column">
-    <button class="button is-small" on:click={resetSearch}>Reset</button>
+  <div class="column is-two-thirds explorer-graph">
+    <NetworkGraph {networkData} {searchItem} {properties} {destroyGraph} />
   </div>
 </div>
 
-{#if loading}
-  <h2 class="title is-2">Loading...</h2>
-{/if}
-
-{#if currentItem}
-  <h2 class="title is-2">{currentLabel} ({currentId})</h2>
-
-  <ItemBasicInfo item={currentItem} languageCodes={languageCodesDisplay} />
-
-  <h3 class="title is-3">Statements</h3>
-  {#each Object.values(currentItem['statements']) as claimProperty}
-    {#each claimProperty as claim (claim.id)}
-      <Claim {claim} />
-    {/each}
-  {/each}
-
-  <h3 class="title is-3">Identifiers</h3>
-  {#each Object.values(currentItem['identifiers']) as claimProperty}
-    {#each claimProperty as claim (claim.id)}
-      <Claim {claim} />
-    {/each}
-  {/each}
-{/if}
-
 <style>
-  /* use css to hide the arrow in <Autocomplete> since hideArrow does not work
-  with bulma
-  https://github.com/pstanoev/simple-svelte-autocomplete/issues/150
-  */
+  .explorer-menu,
+  .explorer-graph {
+    border: 1px solid #bbb;
+  }
+
+  .field {
+    margin-bottom: 1rem;
+  }
+
   :global(.select:not(.is-multiple):not(.is-loading)::after) {
     border: 0;
+  }
+
+  .menu-type {
+    margin-top: 1em;
   }
 </style>
