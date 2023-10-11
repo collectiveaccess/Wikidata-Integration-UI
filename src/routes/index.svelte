7958a5aa1867850d2e1bf19b3b3da31dccd52846
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2023-10-10 23:28:46 -0700

    move title and attribution to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,43 @@
 <script>
-  import NetworkGraph from '../routes/search_graph.svelte';
+  import ADHC_logo from '$lib/images/ADHClogo.png';
+  import ADance_logo from '$lib/images/CapstoneALogo-Dept.TheatreDance.jpg';
+  import NEH_logo from '$lib/images/NEH-Preferred-Seal.jpg';
+  import UT_logo from '$lib/images/RGB_formal_Fine_Arts.svg?raw';
+  import whirl_logo from '$lib/images/whirlLogo.jpg';
+  import SvgIcon from '../lib/components/svg_icon.svelte';
 </script>
 
-<NetworkGraph />
+<h1 class="title is-1">
+  Dancing Digital/No Boundaries Archive Project - Demo Wikidata Integration
+</h1>
+
+<p class="attribution">
+  Co-directors Gesel Mason (University of Texas at Austin) and Rebecca Salzer (University of
+  Alabama). Built by Whirl-i-Gig: Wai-Yin Kwan, Eve Perry, Seth Kaufman
+</p>
+
+<SvgIcon svg={UT_logo} class="logo-svg" />
+<img class="logo" src={ADHC_logo} alt="University of Alabama Digital Humanities Center logo" />
+<img class="logo" src={ADance_logo} alt="University of Alabama Arts and Science logo" />
+<img class="logo" src={NEH_logo} alt="National Endownment for the Humanities logo" />
+<img class="logo" src={whirl_logo} alt="Whirl-i-Gig logo" />
+
+<style>
+  .logo {
+    max-height: 70px;
+    display: inline-block;
+    margin: 5px 10px;
+  }
+
+  :global(.logo-svg) {
+    max-height: 70px;
+    background-color: white;
+    max-width: 400px;
+    margin: 5px 10px;
+  }
+
+  .attribution {
+    margin-bottom: 2rem;
+    font-size: 1.5rem;
+  }
+</style>
